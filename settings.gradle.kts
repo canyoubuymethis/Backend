8a754930479717de83a4506bb31ffe5754122922
ivy <yookyung.lee@carrotins.com>
2023-07-10 11:31:54 +0900

    project setting

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "canyoubuymethis_backend"
---
+++ src/main/kotlin/com/carrot/canyoubuymethis_backend/CanyoubuymethisBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.carrot.canyoubuymethis_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CanyoubuymethisBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<CanyoubuymethisBackendApplication>(*args)
+}
